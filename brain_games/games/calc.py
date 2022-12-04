784115c8a1ecdecc82e207a569546313117e68ea
vitaliy shupegin <vitaliy.shupegin@mail.ru>
2022-12-04 21:37:54 +0300

    update

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,15 +5,15 @@ TASK = 'What is the result of the expression?'
 
 
 def question_answer():
-    num1 = randint(0, 10)
-    num2 = randint(0, 10)
+    num1 = randint(0, 20)
+    num2 = randint(0, 20)
     oper = choice(['+', '-', '*'])
     question = f'{num1} {oper} {num2}'
 
     if oper == '+':
-        correct_answer = num1 + num2
+        correct_answer = (num1 + num2)
     elif oper == '-':
-        correct_answer = num1 - num2
+        correct_answer = (num1 - num2)
     elif oper == '*':
-        correct_answer = num1 * num2
-    return question, correct_answer
+        correct_answer = (num1 * num2)
+    return str(question), correct_answer
