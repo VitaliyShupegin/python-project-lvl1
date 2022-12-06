610e82be41cd2f59db858d6d84ae6292b7836107
vitaliy shupegin <vitaliy.shupegin@mail.ru>
2022-12-06 18:49:06 +0300

    update

---
+++ brain_games/games/progression.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from random import randint, choice
+TASK= 'What number is missing in the progression?'
+
+
+def question_answer():
+    num1 = randint(0, 10)
+    step = randint(1, 10)
+    result = []
+    for i in range(10):
+        next_step = num1 + step * i
+        result.append(str(next_step))
+    elem = choice(range(len(result) - 1))
+    correct_answer = result[elem]
+    result[elem] = '..'
+    question = " ".join(result)
+    return question , correct_answer
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games import progression
+from brain_games.logic import start_game
+
+
+def main():
+    start_game(progression)
+
+
+if __name__ == '__main__':
+    main()
