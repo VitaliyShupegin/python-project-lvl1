61e258dfbbbb2b046a43d9a854d115c10a97f819
vitaliy shupegin <vitaliy.shupegin@mail.ru>
2022-12-14 17:09:55 +0300

    исправленны замечания

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,13 +3,14 @@ from random import randint, choice
 TASK = 'What is the result of the expression?'
 
 
-def get_arithmetic_expression(num1, num2, operator):
+def calc(num1, num2, operator):
     if operator == '+':
-        return num1 + num2
+        result = num1 + num2
     if operator == '-':
-        return num1 - num2
+        result = num1 - num2
     if operator == '*':
-        return num1 * num2
+        result = num1 * num2
+    return result
 
 
 def get_question_and_answer():
@@ -17,5 +18,5 @@ def get_question_and_answer():
     num2 = randint(0, 20)
     operator = choice(['+', '-', '*'])
     question = f'{num1} {operator} {num2}'
-    correct_answer = str(get_arithmetic_expression(num1, num2, operator))
+    correct_answer = str(calc(num1, num2, operator))
     return question, correct_answer
