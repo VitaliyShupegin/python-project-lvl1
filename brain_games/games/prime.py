4a6ac0991397d86536e5adfdef213f14a4906c53
vitaliy shupegin <vitaliy.shupegin@mail.ru>
2022-12-14 16:14:18 +0300

    исправленны замечания

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,14 +4,14 @@ TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(num):
-    if num == 0 or num == 1:
+    if num <= 1:
         return False
     i = 2
-    while i < num:
-        if num % i != 0:
-            i += 1
-        else:
+    while i < num / 2:
+        if num % i == 0:
             return False
+
+        i += 1
     return True
 
 
